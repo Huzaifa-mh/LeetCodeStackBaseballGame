ae4bf066cbf906d2aad51c7ce5d8442a374948b8
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-10-30 20:20:00 +0500

    Update Program.cs

--- LeetCodeStackBaseballGame/Program.cs
+++ LeetCodeStackBaseballGame/Program.cs
@@ -10,7 +10,7 @@
     }
     public int CalPoints(string[] operations)
     {
-        // Stack<char> s1 = new Stack<char>;
+        Stack<char> s1 = new Stack<char>();
         char[] array = new char[operations.Length];
 
         for (int i = 0; i < operations.Length; i++)
@@ -18,14 +18,36 @@
             switch (operations[i])
             {
                 case "+":
-                    if (array.Length > 0)
+                    if (s1.Count > 1)
                     {
-
+                        char l1 = s1.Peek();
+                        s1.Pop();
+                        char l2 = s1.Peek();
+                        s1.Push(l1);
+                        int sum = (l1 - '0') + (l2 - '0');
+                        s1.Push((char)(sum + '0'));
                     }
+                    break;
+                case "D":
+                    if (s1.Count > 0)
+                    {
+                        char last = s1.Peek();
+                        int value = (last - '0') * 2;
+                        s1.Push((char)(value + '0'));
+                    }
+                    break;
+                case "C":
+                    if (s1.Count > 0)
+                    {
+                        s1.Pop();
+                    }
+                    break;
+                default:
+                    char v = operations[i];
+                    s1.Push((char)(v + '0'));
                     break;
             }
         }
 
-        return 0;
     }
 }
