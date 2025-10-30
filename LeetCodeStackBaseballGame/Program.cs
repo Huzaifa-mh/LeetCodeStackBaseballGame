bdb8dedfa47cfd9152c3d0f6270bb5d8e642373a
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-10-30 19:53:33 +0500

    Add project files.

---
+++ LeetCodeStackBaseballGame/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public int CalPoints(string[] operations)
+    {
+        // Stack<char> s1 = new Stack<char>;
+        char[] array = new char[operations.Length];
+
+        for (int i = 0; i < operations.Length; i++)
+        {
+            switch (operations[i])
+            {
+                case '+':
+                    if (array.Length > 0)
+                    {
+
+                    }
+            }
+        }
+    }
+}
